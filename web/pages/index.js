01d32a6e7b4ec68d868e115e5ece17120fff56ca
sarajc <sarajc@stud.ntnu.no>
2022-03-30 18:56:07 +0200

    add web folder

---
+++ web/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+/* const Home() => {
+
+} */
+
+export default function Home() {
+  return (
+    <h1>Hei</h1>
+  )
+}
